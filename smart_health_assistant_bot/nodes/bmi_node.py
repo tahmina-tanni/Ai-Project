50d29e60fbac95d715e16c5f4d1288f4dbff0f70
tahmina-tanni <tahminatanni855@gmail.com>
2026-05-09 20:24:30 +0600

    Delete smart_health_assistant_bot directory

--- smart_health_assistant_bot/nodes/bmi_node.py
+++
@@ -1,7 +0,0 @@
-from logic.health_logic import calculate_bmi_data
-
-def bmi_node(state):
-    if state.get("is_finished"):
-        bmi, cat, tip = calculate_bmi_data(float(state["weight"]), float(state["height"]))
-        return {"bmi_score": bmi, "bmi_cat": cat, "advice": tip}
-    return {}
